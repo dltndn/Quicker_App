c7ff926132c349d16d7f2ca081dfddb9d88f6b9a
dltndn <james98099@gmail.com>
2023-01-21 12:45:46 +0900

    test

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const a:String = "as";
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Text>quicker start</Text>
-      <Text>quicker start test</Text>
+      <Text>{a}</Text>
     </View>
   );
 }
